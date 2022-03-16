51910849bd196740f465ab1f6ca8bbeb998eead1
s1uad-dw <s1uad.dw@gmail.com>
2022-03-16 17:39:58 +0300

    tournament class created

--- tournament/models.py
+++ tournament/models.py
@@ -1,3 +1,11 @@
+import imp
 from django.db import models
 
-# Create your models here.
+class Tournament(models.Model):
+    name = models.CharField('Tournament name', max_length = 75)
+    start_date = models.DateTimeField('Date fo start')
+    status = models.BooleanField('Status')
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
