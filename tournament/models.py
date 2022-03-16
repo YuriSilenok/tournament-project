d7a69f164f4a78c7a61c86dcee9e7aa7a0eeacf3
s1uad-dw <s1uad.dw@gmail.com>
2022-03-16 18:25:34 +0300

    second try

--- tournament/models.py
+++ tournament/models.py
@@ -1,11 +1,14 @@
-import imp
 from django.db import models
+from django.utils import timezone
 
 class Tournament(models.Model):
     name = models.CharField('Tournament name', max_length = 75)
-    start_date = models.DateTimeField('Date fo start')
-    status = models.BooleanField('Status')
+    start_date = models.DateTimeField('Date of start', default=timezone.now())
+    completeness = models.BooleanField('Completeness of tournament', default=False)
 
     class Meta:
-        verbose_name = "Статья"
-        verbose_name_plural = "Статьи"
+        verbose_name = "Турнир"
+        verbose_name_plural = "Турниры"
+        
+    def __str__(self):
+        return self.name
