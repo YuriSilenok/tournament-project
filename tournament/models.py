09c07f02075b4eeb1d8e609b579c5e76dfafebf5
Даниил <kourin.daniil@gmail.com>
2022-03-17 10:29:45 +0300

    Third try

--- tournament/models.py
+++ tournament/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.utils import timezone
+
 
 class Tournament(models.Model):
-    name = models.CharField('Tournament name', max_length = 75)
-    start_date = models.DateTimeField('Date of start', default=timezone.now())
+    name = models.CharField('Tournament name', max_length=75)
+    start_date = models.DateField('Date of start', auto_now=True)
     completeness = models.BooleanField('Completeness of tournament', default=False)
 
     class Meta:
         verbose_name = "Турнир"
         verbose_name_plural = "Турниры"
-        
+
     def __str__(self):
         return self.name
