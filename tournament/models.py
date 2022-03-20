072ee0b6095436f3179ad4f884b80f43d33c1a22
Mamoru_Kawai <mamorubelka@gmail.com>
2022-03-20 12:01:49 +0300

    Create model Human

--- tournament/models.py
+++ tournament/models.py
@@ -34,3 +34,38 @@ class Nomination(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Human(models.Model):
+    surname = models.CharField('Surname', max_length = 50)
+    name = models.CharField('Name', max_length = 50)
+    patronymic = models.CharField('Patronymic', max_length = 50)
+    year_of_birth = models.CharField('Year', max_length = 4)
+    gender = models.CharField("Gender", max_length = 50)
+    
+    class Meta:
+    
+        verbose_surname = "Фамилия"
+        verbose_name_plural_surname = "Фамилии"
+        verbose_name = "Имя"
+        verbose_name_plural = "Имена"
+        verbose_patronymic = "Отчество"
+        verbose_year_of_birth = "Год рождения"
+        verbose_gender = "Пол"
+    
+    def __str__(self, value):
+       
+        if value == "surname":
+            return self.surname
+
+        elif value == "name":
+            return self.name
+
+        elif value == "patronymic":
+            return self.patronymic
+
+        elif value == "year_of_birth":
+            return self.year_of_birth
+
+        else:
+            return self.gender
