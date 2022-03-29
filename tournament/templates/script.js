0bdd50cd92efb405b3fc70466e49dd1446d74dbd
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2022-03-29 23:46:57 +0400

    tournament list

--- tournament/templates/script.js
+++ tournament/templates/script.js
@@ -1,19 +1,9 @@
+list =  new XMLHttpRequest();
 //ссылка на базу дынных
-//list.open(method='get','../../db.sqlite3');
-//list.responseType = 'json';
-//иммитируем получение
-
-var list = {
-    "Tournament":[
-	{"name":"Турнир 1", "id":"0","status":"Завершён","date":"20.03.22"},
-	{"name":"Турнир 2", "id":"1","status":"Завершён","date":"20.03.22"},
-	{"name":"Турнир 3", "id":"2","status":"Завершён","date":"20.03.22"},
-	{"name":"Турнир 4", "id":"4","status":"Не Завершён","date":"20.03.22"},
-	{"name":"Турнир 5", "id":"6","status":"Не Завершён,","date":"20.03.22"}]
-};
+list.open(method='get','../../tournament-project.sqlite3');
+list.responseType = 'json';
   x = list.Tournament[0];
  for (i in list.Tournament) {
     x = list.Tournament[i].name;
 	document.getElementById('tournament_list').innerHTML += x.link(list.Tournament[i].id) +"<br/>" ;
  };
- //list.send();
