ffbf8770b6a72e8ecdd8fb41bab3615f961aa3ab
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2022-04-01 19:55:56 +0400

    fixed  some mistakes

--- tournament/templates/script.js
+++ tournament/templates/script.js
@@ -1,8 +1,9 @@
 list =  new XMLHttpRequest();
 //ссылка на базу дынных
-list.open(method='get','../../tournament-project.sqlite3');
+list.open(method='get','../tournament-project.sqlite3');
 list.responseType = 'json';
-  x = list.Tournament[0];
+
+ x = list.Tournament[0];
  for (i in list.Tournament) {
     x = list.Tournament[i].name;
 	document.getElementById('tournament_list').innerHTML += x.link(list.Tournament[i].id) +"<br/>" ;
